6811567846b9bf18310d4502010e58f9ec44b5e6
kaiyrkeldi_sagitzan <adil_groud@mail.ru>
2024-04-21 21:51:19 +0500

    first commit constructors add addFirst get get First/Last iterator

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,26 @@ import javax.lang.model.element.Element;
 import java.lang.annotation.ElementType;
 import java.nio.IntBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        MyArrayList<Integer> arr = new MyArrayList<Integer>(2);
-        int[] b = new int[20];
-        for(int i=0;i<20;i++){
+        MyArrayList<Integer> arr = new MyArrayList<Integer>();
+        ArrayList<Integer> a = new ArrayList<>();
+
+        int[] b = new int[37];
+        for(int i=1;i<37;i++){
             b[i]=i;
         }
-        arr.add()
-        arr.add(a);
-        System.out.println(arr.indexOf(4));
-
-
-
+        for(int i=0;i<36;i++){
+            arr.add(b[i]);
+        }
+//        for(int i=0;i<20;i++){
+//            System.out.println(arr.get(i));
+//        }
+        System.out.println(arr.exists(56));
 //        int a = in.nextInt();
 //        int b = in.nextInt();
 //
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,82 +1,68 @@
+import java.util.Arrays;
 import java.util.Iterator;
 
-public class MyArrayList<T> implements MyList<T>{
+public class MyArrayList <T extends Comparable<T>> implements MyList<T> {
 
     private static final int default_capacity = 10;
-
-    private static final Object[] empty_data={};
-    public Object[] element_data;
-    private static int size=0;
-    private int capacity ;
+    private Object[] element_data;
+    private static int size = 0;
 
 
-    public MyArrayList(){
-        element_data=new Object[default_capacity];
-        capacity = default_capacity;
+    public MyArrayList() {
+        element_data = new Object[default_capacity];
     }
-    public MyArrayList(int initialCapacity){
-        if(initialCapacity==0){
-            capacity=0;
-            element_data=empty_data;
-        }
-        else if(initialCapacity>0){
-            element_data=new Object[initialCapacity];
-            capacity=initialCapacity;
-        }
-        else {
+
+    public MyArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            element_data = new Object[initialCapacity];
+        } else {
             throw new IllegalArgumentException("capacity can't be a negative number :" + initialCapacity);
         }
 
     }
 
 
-
     @Override
     public void add(T item) {
-        if(element_data.length==size+1){
+        if (element_data.length == size + 1) {
             increaseCapacity();
-
         }
 
-        element_data[size]=item;
+        element_data[size] = item;
         size++;
     }
-    public void increaseCapacity(){
-        capacity = capacity+1;
-        Object[] temp = new Object[capacity];
-        for(int i=0;i<size;i++){
-            temp[i]=element_data[i];
+
+    public void increaseCapacity() {
+        Object[] temp = new Object[element_data.length * 2];
+        for (int i = 0; i < size; i++) {
+            temp[i] = element_data[i];
         }
-        element_data=temp;
+        element_data = temp;
     }
-
-
-
-
 
 
     @Override
     public void set(int index, T item) {
-        element_data[index]= item;
+        element_data[index] = item;
     }
 
     @Override
     public void add(int index, T item) {
-        element_data[index]=item;
+        element_data[index] = item;
         size++;
     }
 
     @Override
     public void addFirst(T item) {
-        if(size >= capacity){
+        if (size >= element_data.length) {
             increaseCapacity();
         }
-        Object[] temp = new Object[element_data.length+1];
-        temp[0]= item;
-        for(int i=1;i<element_data.length;i++){
-            temp[i]=element_data[i-1];
+        Object[] temp = new Object[element_data.length + 1];
+        temp[0] = item;
+        for (int i = 1; i < element_data.length; i++) {
+            temp[i] = element_data[i - 1];
         }
-        element_data=temp;
+        element_data = temp;
         size++;
     }
 
@@ -87,7 +73,7 @@ public class MyArrayList<T> implements MyList<T>{
 
     @Override
     public T get(int index) {
-        if(index>capacity){
+        if (index >= size) {
             System.out.println("out of length");
         }
         return (T) element_data[index];
@@ -100,7 +86,7 @@ public class MyArrayList<T> implements MyList<T>{
 
     @Override
     public T getLast() {
-        return (T) element_data[size-1];
+        return (T) element_data[size - 1];
     }
 
     @Override
@@ -108,100 +94,118 @@ public class MyArrayList<T> implements MyList<T>{
 
         Object[] temp = new Object[element_data.length];
 
-        for(int i =0; i< index;i++){
+        for (int i = 0; i < index; i++) {
             temp[i] = element_data[i];
         }
 
-        for(int i=index ;i<size-1;i++){
-            temp[i]=element_data[i+1];
+        for (int i = index; i < size - 1; i++) {
+            temp[i] = element_data[i + 1];
         }
         size--;
-        element_data=temp;
+        element_data = temp;
 
     }
 
     @Override
     public void removeFirst() {
-        Object[] temp = new Object[size-1];
+        Object[] temp = new Object[size - 1];
 
-        for(int i=0;i<size-1;i++){
-            temp[i]=element_data[i+1];
+        for (int i = 0; i < size - 1; i++) {
+            temp[i] = element_data[i + 1];
         }
-        element_data=temp;
+        element_data = temp;
         size--;
     }
 
     @Override
     public void removeLast() {
-        Object[] temp = new Object[size-1];
-        for(int i =0 ;i<size-1;i++){
-            temp[i]=element_data[i];
+        Object[] temp = new Object[size - 1];
+        for (int i = 0; i < size - 1; i++) {
+            temp[i] = element_data[i];
         }
-        element_data =temp ;
+        element_data = temp;
         size--;
     }
 
     @Override
     public void sort() {
-
-    }
-
-    @Override
-    public int indexOf(Object object) {
-        for(int i=0;i<size;i++){
-            if(element_data[i]==object){
-                return i;
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size - i - 1; j++) {
             }
         }
-        return -1;
     }
 
-    @Override
-    public int lastIndexOf(Object object) {
-        for(int i = size; i>=0; i--) {
-            if (element_data[i] == object) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    @Override
-    public boolean exists(Object object) {
-        return false;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public void clear() {
-        Object[] temp = new Object[default_capacity];
-        element_data=temp;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new MyIterator();
-    }
-
-    public class MyIterator implements Iterator<T>{
-        private int index =0;
         @Override
-        public boolean hasNext() {
-            return index<size;
+        public int indexOf (Object object){
+            for (int i = 0; i < size; i++) {
+                if (element_data[i] == object) {
+                    return i;
+                }
+            }
+            return -1;
         }
 
         @Override
-        public T next() {
-            return (T) element_data[index++];
+        public int lastIndexOf (Object object){
+            for (int i = size; i >= 0; i--) {
+                if (element_data[i] == object) {
+                    return i;
+                }
+            }
+            return -1;
         }
-    }
+
+        @Override
+        public boolean exists (Object object){
+            int check = 0;
+            for (int i = 0; i < size; i++) {
+                if (element_data[i] == object) {
+                    check++;
+                }
+
+            }
+            if (check > 0) {
+                return true;
+            } else return false;
+        }
+
+        @Override
+        public Object[] toArray () {
+            Object[] temp = new Object[size];
+            for (int i = 0; i < size; i++) {
+                temp[i] = element_data[i];
+            }
+            return temp;
+        }
+
+        @Override
+        public void clear () {
+            Object[] temp = new Object[default_capacity];
+            element_data = temp;
+        }
+
+        @Override
+        public int size () {
+            return size;
+        }
+
+        @Override
+        public Iterator<T> iterator () {
+            return new MyIterator();
+        }
+
+        public class MyIterator implements Iterator<T> {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                return (T) element_data[index++];
+            }
+        }
+
 }
